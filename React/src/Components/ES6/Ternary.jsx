bba2e0c9f53009b1fe59f8fc1034c4d32b1b9071
Chriskunda <christianira2003@gmail.com>
2025-05-08 10:36:47 +0200

    Modified Tenary

--- React/src/Components/ES6/Ternary.jsx
+++ React/src/Components/ES6/Ternary.jsx
@@ -11,14 +11,16 @@ const Ternary = () => {
     const NormalCheck = () => {
         if(a > b) {
             return (<h1>a is greater than b</h1>)
+        } else if (a < b) {
+            return(<h1>b is greater than a</h1>)
         } else {
-            return (<h1>b is greater or equal to a</h1>)
+            return (<h1>b is equal to a</h1>)
         }
     }
 
     const TernaryCheck = () => {
         return (
-            a > b ? <h1>a is greater than b</h1> : <h1>b is greater or equal to a</h1>
+            a > b ? <h1>a is greater than b</h1> : a < b ? <h1>b is greater than a</h1> : <h1>a & b are equal</h1>
         )
     }
 
